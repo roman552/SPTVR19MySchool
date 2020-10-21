c961bb9e47174b506112028d5a4fe8d1bae870ca
roman ivanov <roman.ivanov@ivkhk.ee>
2020-10-21 10:28:15 +0300

    Заготовка проекта

---
+++ src/sptvr19myschool/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19myschool;
+
+/**
+ *
+ * @author user
+ */
+class App {
+
+    public void run() {
+        System.out.println("---Моя школа---");
+        
+    }
+    
+}
